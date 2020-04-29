420bc167cee1940d9fbebac506f58f75b266e958
Luis <cifuentes_estrada@hotmail.com>
2020-04-28 21:19:07 -0600

    FORM

--- src/app.js
+++ src/app.js
@@ -1,10 +1,63 @@
-import { showAlert } from './messages';
-import $ from 'jquery';
-import "./styles.scss";
-import uuid from 'uuid/v4'
+import './styles/chart.scss'
+/* Imports */
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_moonrisekingdom from "@amcharts/amcharts4/themes/moonrisekingdom";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-document.getElementById('btn-alert')
-    .addEventListener('click', showAlert);
+import am4themes_dark from "@amcharts/amcharts4/themes/dark";
 
-$('#btn-jq').click(() => alert(uuid()))
+/* Chart code */
+// Themes begin
+am4core.useTheme(am4themes_moonrisekingdom);
+am4core.useTheme(am4themes_animated);
+// Themes end
 
+// Create chart instance
+let chart = am4core.create("chartdiv", am4charts.PieChart);
+
+// Add data
+chart.data = [ {
+    "country": "Lithuania",
+    "litres": 501.9
+}, {
+    "country": "Czech Republic",
+    "litres": 301.9
+}, {
+    "country": "Ireland",
+    "litres": 201.1
+}, {
+    "country": "Germany",
+    "litres": 165.8
+}, {
+    "country": "Australia",
+    "litres": 139.9
+}, {
+    "country": "Austria",
+    "litres": 128.3
+}, {
+    "country": "UK",
+    "litres": 99
+}, {
+    "country": "Belgium",
+    "litres": 60
+}, {
+    "country": "The Netherlands",
+    "litres": 50
+} ];
+
+// Set inner radius
+chart.innerRadius = am4core.percent(50);
+
+// Add and configure Series
+let pieSeries = chart.series.push(new am4charts.PieSeries());
+pieSeries.dataFields.value = "litres";
+pieSeries.dataFields.category = "country";
+pieSeries.slices.template.stroke = am4core.color("#fff");
+pieSeries.slices.template.strokeWidth = 2;
+pieSeries.slices.template.strokeOpacity = 1;
+
+// This creates initial animation
+pieSeries.hiddenState.properties.opacity = 1;
+pieSeries.hiddenState.properties.endAngle = -90;
+pieSeries.hiddenState.properties.startAngle = -90;
---
+++ src/utilidades/filter_struc.js
@@ -0,0 +1,65 @@
+export const index_schema = {
+    se_pueblos: {
+        1: "Maya",
+        2: "Garifuna",
+        3: "Xinca",
+        4: "Mestizo",
+        5: "Otros",
+    },
+    se_comunidad_linguistica: {
+        1: "Achí",
+        2: "Akateka",
+        3: "Awakateka",
+        4: "Chalchiteka",
+        5: "Ch´orti´",
+        6: "Chuj",
+        7: "Itza´",
+        8: "Ixil",
+        9: "Jakalteka (Popti´)",
+        10: "Kaqchikel",
+        11: "K´iche´",
+        12: "Mam",
+        13: "Mopan",
+        14: "Poqoman",
+        15: "Poqomchi´",
+        16: "Q´anjob´al",
+        17: "Q´eqchi´",
+        18: "Sakapulteka",
+        19: "Sipakapense",
+        20: "Tektiteka"
+    },
+    se_sector: {
+        1: "Servicios",
+        2: "Comercio",
+        3: "Agroindustria",
+        4: "Industria",
+        5: "Artesanías",
+        6: "Gobierno",
+        7: "Academia",
+        8: "Tecnología"
+    },
+    se_dicapacidad: {
+        1: "Sin discapacidad",
+        2: "Discapacidad Visual",
+        3: "Discapacidad Auditiva",
+        4: "Discapacidad Psicosocial",
+        5: "Androplasia",
+        6: "Discapacidad Física",
+        7: "Discapacidad Intelectual"
+    },
+    se_localidad: {
+        1: "Rural",
+        2: "Urbano"
+    },
+    se_edad: {
+        1: "0-13",
+        2: "14-30",
+        3: "31-60",
+        4: "61 en adelante",
+    },
+    sexo: {
+        "M": "MUJERES",
+        "H": "HOMBRES",
+        "ND": "ND"
+    }
+}
